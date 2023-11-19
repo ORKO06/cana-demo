33f0fec81bda00848ac1e53c7f721fd93a636104
Hemant Bajaj <hemantbajaj@Hemants-MacBook-Air.local>
2023-11-20 00:57:31 +0530

    chore: update app.py and upload joblib

--- Api/app.py
+++ Api/app.py
@@ -1,13 +1,17 @@
 import flask
-import pickle
+import joblib
 import pandas as pd
 import flask_cors
 from flask import request, jsonify, make_response
 
 # Use pickle to load in the pre-trained model.
 # Have a folder model and inside that have the pkl file loaded
-with open(f'model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# with open(f'model.pkl', 'rb') as f:
+# model = pickle.load(f)
+
+model = joblib.load('final_model.joblib')
+
+print(type(model))
 
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
@@ -26,33 +30,36 @@ def mai():
 
 @app.route('/re', methods=['GET', 'POST'])
 def main():
-    if flask.request.method == 'OPTIONS':
-        response = flask.Response()
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Methods', 'POST')
-        return response
 
-    if flask.request.method == 'GET':
-        # Just render the initial form, to get input
-        return (flask.render_template('./main.html'))
+    # if flask.request.method == 'GET':
+    #     # Just render the initial form, to get input
+    #     response = make_response("Hellod", 200)
+    #     return response
 
     if flask.request.method == 'POST':
-        data = request.get_json()
-        N_Default_L3m = data['N_Default_L3m'].value
-        Max_Utilization = data['Max_Utilization'].value
-        Max_Perc_Def_Chg_Pending = data['Max_Perc_Def_Chg_Pending'].value
-        N_Family_Member = data['N_Family_Member'].value
-        N_PosBkt_L3m = data['N_PosBkt_L3m'].value
-        Ever_Default_L12M = data['Ever_Default_L12M'].value
-        Perc_Paymode_Online = data['Perc_Paymode_Online'].value
-        Perc_Repay_Fail = data['Perc_Repay_Fail'].value
-        Max_DPD_L3m = data['Max_DPD_L3m'].value
-        Perc_Paymode_Cheq_Fail = data['Perc_Paymode_Cheq_Fail'].value
-        Age = data['Age'].value
-        N_Enq_L9m = data['N_Enq_L9m'].value
-        Max_Loan_Balance_Others = data['Max_Loan_Balance_Others'].value
-        N_WorkEx_Yr = data['N_WorkEx_Yr'].value
+        # data = request.get_json()
+        # print(data)
+        # 3.0	38	100.0	5	0.0	1	0.285714	0.428571	19.0	0.750000	46	0	5527.0	15
+        N_Default_L3m = 2
+        Max_Utilization = 100
+        Max_Perc_Def_Chg_Pending = 100
+        N_Family_Member = 1
+        N_PosBkt_L3m = 0
+        Ever_Default_L12M = 1
+        Perc_Paymode_Online = 0.333333333
+        Perc_Repay_Fail = 0.333333333
+        Max_DPD_L3m = 19
+        Perc_Paymode_Cheq_Fail = 0.5
+        Age = 24
+        N_Enq_L9m = 0
+        Max_Loan_Balance_Others = 55384
+        N_WorkEx_Yr = 4
+
+        Max_DPD_L3m = Max_DPD_L3m*Max_DPD_L3m
+        N_Default_L3m = N_Default_L3m*N_Default_L3m
+        Perc_Repay_Fail = Perc_Repay_Fail*Perc_Repay_Fail
+        Max_Utilization = Max_Utilization*Max_Utilization
+        N_WorkEx_Yr = N_WorkEx_Yr*N_WorkEx_Yr
 
         columns = [
             "N_Default_L3m",
@@ -77,14 +84,20 @@ def main():
                                         Max_Loan_Balance_Others, N_WorkEx_Yr]],
                                        columns=columns,
                                        dtype=float)
-        prediction = model.predict_proba(input_variables)[:, 1]
 
+        if hasattr(model, 'predict_proba') and callable(getattr(model, 'predict_proba')):
+            prediction = model.predict_proba(input_variables)[:, 1]
+            response = flask.jsonify({'result': prediction.tolist()})
+            response.headers.add('Content-Type', 'application/json')
+        else:
+            response = flask.jsonify(
+                {'error': 'Model does not support predict_proba'})
+            response.headers.add('Content-Type', 'application/json')
+
+        # print(getattr(model, 'predict_probability'))
+        return response
         # Render the form again, but add in the prediction and remind user
         # of the values they input before
-        response = flask.jsonify({'result': prediction.tolist()})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Methods', 'POST')
 
         # Render the form again, but add in the prediction and remind the user
         # of the values they input before
