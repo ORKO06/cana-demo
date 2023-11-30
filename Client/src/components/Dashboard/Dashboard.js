0b4cc76373f4c0395da7bb89097434d1d2ddc5f0
Hemant Bajaj <hemantbajaj@Hemants-MacBook-Air.local>
2023-11-30 09:57:02 +0530

    feat: add api handling

--- Client/src/components/Dashboard/Dashboard.js
+++ Client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 // static 5 year rainfall ka data
 // Show static insurance and Mutual fund(Canara bank has khudka) products images
 
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import last5YearRainfall from '../../assets/5yearrainfall.png'
 import forecast from '../../assets/forecast.jpeg'
 import policy1 from '../../assets/policy1.jpeg'
@@ -15,22 +15,52 @@ import axios from 'axios'
 
 const Dashboard = () => {
     const { inputData } = useContext(RegistrationContext);
+    const [predict, setPredict] = useState(0);
 
     useEffect(()=>{
-        const data = {...inputData};
+        const data = {
+            Branch_Code: {value: inputData.Branch_Code.value},
+            CoAp_Income: {value: inputData.CoAp_Income.value},
+            Customer_No: {value: inputData.Customer_No.value},
+            Ever_Default_L12M: {value: inputData.Ever_Default_L12M.value},
+            Max_DPD_L3m: {value: inputData.Max_DPD_L3m.value},
+            Max_Loan_Balance_Others: {value: inputData.Max_Loan_Balance_Others.value},
+            Max_Perc_Def_Chg_Pending: {value: inputData.Max_Perc_Def_Chg_Pending.value},
+            Max_Ratio_OC_Pending_POS: {value: inputData.Max_Ratio_OC_Pending_POS.value},
+            Max_Utilization: {value: inputData.Max_Utilization.value},
+            N_Default_L3m: {value: inputData.N_Default_L3m.value},
+            N_Enq_L9m: {value: inputData.N_Enq_L9m.value},
+            N_Family_Member:{value: inputData.N_Family_Member.value},
+            Age: {value: inputData.Age.value},
+            N_PosBkt_L3m: {value: inputData.N_PosBkt_L3m.value},
+            N_WorkEx_Yr: {value: inputData.N_WorkEx_Yr.value},
+            Perc_Paymode_Cheq_Fail: {value: inputData.Perc_Paymode_Cheq_Fail.value},
+            Perc_Paymode_Online: {value: inputData.Perc_Paymode_Online.value},
+            Perc_Repay_Fail: {value: inputData.Perc_Repay_Fail.value},
+            Total_Field_Trails: {value: inputData.Total_Field_Trails.value},
+            Total_Resolved: {value: inputData.Total_Resolved.value},
+            }
         console.log(data);
-        axios.post('https://canara.onrender.com/re', data ).then(response => {
-            console.log('Response:', response.data);
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
+        const config = {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          };
+        axios.post('https://canara.onrender.com/re', data, config )
+        .then(response => {
+            console.log('Response:', response.data.result[0]);
+            setPredict(Number((response.data.result[0] * 100).toFixed(2)))
+        })
+        .catch(error => {
+          console.error('Error:', error.message); 
+          console.error('Error Details:', error.response.data); 
+        });
     },[inputData])
 
   console.log(inputData);
   return (
     <DashboardContainer>
-        <PredictionText>Prediction: 70%</PredictionText>
+        <PredictionText>Prediction: {predict}%</PredictionText>
         <ImageContainer>
             <img height={200} width={400} src={last5YearRainfall} alt="Text" />
             <img height={200} width={400} src={forecast} alt="Text" />
